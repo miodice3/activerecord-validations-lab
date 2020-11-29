fe30235d4642d19b2fec56f39fe09028aeea05b0
Michael Iodice <miodice3@gmail.com>
2020-11-29 08:31:17 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
 end
+# validates :name, presence: true
+# validates :name, uniqueness: true
+# validates :phone_number, length: { is: 10 }
+# validates :name, length: { minimum: 2 }
+# validates :bio, length: { maximum: 500 }
+# validates :password, length: { in: 6..20 }
+# validates :registration_number, length: { is: 6 }
