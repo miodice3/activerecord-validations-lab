c7ed460a3cd5fc4e7ff561303ddefdfa16720684
Michael Iodice <miodice3@gmail.com>
2020-11-29 08:59:28 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,21 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
-    message: "%{value} is not a valid category" }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait?
+  
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+  
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
 # validates :name, presence: true
 # validates :name, uniqueness: true
@@ -12,3 +25,5 @@ end
 # validates :bio, length: { maximum: 500 }
 # validates :password, length: { in: 6..20 }
 # validates :registration_number, length: { is: 6 }
+
+#expected `#<Post id: nil, title: "True Facts", content: "Lorem ipsum dolor sit amet, consectetur adipiscing...", summary: "Abstract.", category: "Fiction", created_at: nil, updated_at: nil>.invalid?` to return true, got false
